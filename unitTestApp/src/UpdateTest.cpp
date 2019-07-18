9d2ebef2c67dd3fe76d894b510cf049965440c49
Ralph Lange <ralph.lange@gmx.de>
2019-07-18 13:13:26 +0200

    Refactor Update/UpdateQueue; add status

--- unitTestApp/src/UpdateTest.cpp
+++ unitTestApp/src/UpdateTest.cpp
@@ -14,20 +14,23 @@
 
 #include <epicsTime.h>
 
-#include "UpdateQueue.h"
+#include "Update.h"
 
 namespace {
 
 using namespace DevOpcua;
 
+typedef Update<int, unsigned short> TestUpdate;
+
 TEST(UpdateTest, UpdateConstructor_WithDataRef_DataCorrectAndManaged) {
     epicsTime ts;
     ts.getCurrent();
 
-    Update<int> u0(ts, ProcessReason::incomingData, 1);
+    TestUpdate u0(ts, ProcessReason::incomingData, 1, 101);
     EXPECT_EQ(u0.getOverrides(), 0ul) << "New Update has override counter > 0";
     EXPECT_EQ(u0.getTimeStamp(), ts) << "New Update time stamp differs from the provided stamp";
     EXPECT_EQ(u0.getType(), ProcessReason::incomingData) << "New Update ProcessReason differs from the provided type";
+    EXPECT_EQ(u0.getStatus(), 101u) << "New Update status differs from the provided status";
     EXPECT_EQ(bool(u0), true) << "New Update (created with Data Reference) does not have data (operator bool returns false)";
 
     int i = u0.getData();
@@ -45,10 +48,11 @@ TEST(UpdateTest, UpdateConstructor_WithUniquePtr_DataCorrectAndManaged) {
 
     std::unique_ptr<int> pin(new int(1));
 
-    Update<int> u0(ts, ProcessReason::readComplete, std::move(pin));
+    TestUpdate u0(ts, ProcessReason::readComplete, std::move(pin), 101);
     EXPECT_EQ(u0.getOverrides(), 0ul) << "New Update has override counter > 0";
     EXPECT_EQ(u0.getTimeStamp(), ts) << "New Update time stamp differs from the provided stamp";
     EXPECT_EQ(u0.getType(), ProcessReason::readComplete) << "New Update ProcessReason differs from the provided type";
+    EXPECT_EQ(u0.getStatus(), 101u) << "New Update status differs from the provided status";
     EXPECT_EQ(bool(u0), true) << "New Update (created with unique_ptr) does not have data (operator bool returns false)";
 
     int i = u0.getData();
@@ -64,10 +68,11 @@ TEST(UpdateTest, UpdateConstructor_NoData_DataEmpty) {
     epicsTime ts;
     ts.getCurrent();
 
-    Update<int> u0(ts, ProcessReason::connectionLoss);
+    TestUpdate u0(ts, ProcessReason::connectionLoss);
     EXPECT_EQ(u0.getOverrides(), 0ul) << "New Update has override counter > 0";
     EXPECT_EQ(u0.getTimeStamp(), ts) << "New Update time stamp differs from the provided stamp";
     EXPECT_EQ(u0.getType(), ProcessReason::connectionLoss) << "New Update ProcessReason differs from the provided type";
+    EXPECT_EQ(u0.getStatus(), 0) << "New Update status differs from the provided status";
     EXPECT_EQ(bool(u0), false) << "New Update (created without data) does have data (operator bool returns true)";
 
     std::unique_ptr<int> pi(u0.releaseData());
@@ -78,16 +83,17 @@ TEST(UpdateTest, override_2xWithUpdate_OverriddenDataCorrect) {
     int i;
     epicsTime ts0;
     ts0.getCurrent();
-    Update<int> u0(ts0, ProcessReason::incomingData, 0);
+    TestUpdate u0(ts0, ProcessReason::incomingData, 0, 100);
     epicsTime ts1 = ts0 + 1.0;
-    Update<int> u1(ts1, ProcessReason::writeComplete, 1);
+    TestUpdate u1(ts1, ProcessReason::writeComplete, 1, 101);
     epicsTime ts2 = ts0 + 2.0;
-    Update<int> u2(ts2, ProcessReason::readComplete, 2);
+    TestUpdate u2(ts2, ProcessReason::readComplete, 2, 102);
 
     u0.override(u1);
     EXPECT_EQ(u0.getOverrides(), 1ul) << "Update override counter (" << u0.getOverrides() << ") expected 1";
     EXPECT_EQ(u0.getTimeStamp(), ts1) << "Update timestamp is not set from override";
     EXPECT_EQ(u0.getType(), ProcessReason::writeComplete) << "Update ProcessReason is not set from override";
+    EXPECT_EQ(u0.getStatus(), 101u) << "Update status is not set from override";
     EXPECT_EQ(bool(u0), true) << "Update (after override 1) does not have data (operator bool returns false)";
     i = u0.getData();
     EXPECT_EQ(i, 1) << "Update data (" << i << ") differs from override data (1)";
@@ -97,6 +103,7 @@ TEST(UpdateTest, override_2xWithUpdate_OverriddenDataCorrect) {
     EXPECT_EQ(u0.getOverrides(), 2ul) << "Update override counter (" << u0.getOverrides() << ") expected 2";
     EXPECT_EQ(u0.getTimeStamp(), ts2) << "Update timestamp is not set from override";
     EXPECT_EQ(u0.getType(), ProcessReason::readComplete) << "Update ProcessReason is not set from override";
+    EXPECT_EQ(u0.getStatus(), 102) << "Update status is not set from override";
     i = u0.getData();
     EXPECT_EQ(i, 2) << "Update data (" << i << ") differs from override data (2)";
     EXPECT_EQ(bool(u2), false) << "Override Update still has data (operator bool returns true)";
@@ -106,12 +113,13 @@ TEST(UpdateTest, override_2xWithCounter_OverriddenDataCorrect) {
     int i;
     epicsTime ts0;
     ts0.getCurrent();
-    Update<int> u0(ts0, ProcessReason::incomingData, 1);
+    TestUpdate u0(ts0, ProcessReason::incomingData, 1, 101);
 
     u0.override(2);
     EXPECT_EQ(u0.getOverrides(), 3ul) << "Update override counter (" << u0.getOverrides() << ") is not 3";
     EXPECT_EQ(u0.getTimeStamp(), ts0) << "Update timestamp changed";
     EXPECT_EQ(u0.getType(), ProcessReason::incomingData) << "Update ProcessReason changed";
+    EXPECT_EQ(u0.getStatus(), 101u) << "Update status changed";
     i = u0.getData();
     EXPECT_EQ(i, 1) << "Update data (" << i << ") changed";
 
@@ -119,6 +127,7 @@ TEST(UpdateTest, override_2xWithCounter_OverriddenDataCorrect) {
     EXPECT_EQ(u0.getOverrides(), 7ul) << "Update override counter (" << u0.getOverrides() << ") is not 7";
     EXPECT_EQ(u0.getTimeStamp(), ts0) << "Update timestamp changed";
     EXPECT_EQ(u0.getType(), ProcessReason::incomingData) << "Update ProcessReason changed";
+    EXPECT_EQ(u0.getStatus(), 101u) << "Update status changed";
     i = u0.getData();
     EXPECT_EQ(i, 1) << "Update data (" << i << ") changed";
 }
