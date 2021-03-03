adb784ee1ee780f0ed99b82d7dc771b799587c29
Ralph Lange <ralph.lange@gmx.de>
2021-03-03 19:26:26 +0100

    example: use Linux version of st.cmd
    - fixes #90

--- exampleTop/iocBoot/iocopcuaEx/st.cmd
+++ exampleTop/iocBoot/iocopcuaEx/st.cmd
@@ -3,11 +3,13 @@
 ## You may have to change opcuaIoc to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
+
+cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase("../../dbd/opcuaIoc.dbd",0,0)
-opcuaIoc_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase "dbd/opcuaIoc.dbd"
+opcuaIoc_registerRecordDeviceDriver pdbbase
 
 ## Pretty minimal setup: one session with a 200ms subscription on top
 opcuaCreateSession OPC1 opc.tcp://localhost:4840
@@ -16,10 +18,10 @@ opcuaCreateSubscription SUB1 OPC1 200
 ## Load the databases for one of the examples
 
 ## Siemens S7-1500 PLC
-dbLoadRecords("../../db/S7-1500-server.db","P=OPC:,R=,SESS=OPC1,SUBS=SUB1")
-dbLoadRecords("../../db/S7-1500-DB1.db","P=OPC:,R=DB1:,SESS=OPC1,SUBS=SUB1")
+dbLoadRecords "db/S7-1500-server.db", "P=OPC:,R=,SESS=OPC1,SUBS=SUB1"
+dbLoadRecords "db/S7-1500-DB1.db", "P=OPC:,R=DB1:,SESS=OPC1,SUBS=SUB1"
 
-iocInit()
+iocInit
 
 ## Start any sequence programs
 #seq sncopcuaIoc,"user=ralph"
