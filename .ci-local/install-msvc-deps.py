1b4b764f40ff76d8c6617cb45682a4ea259061b9
Ralph Lange <ralph.lange@gmx.de>
2024-07-23 17:01:34 +0200

    ci: improve MSVC dependency downloader

--- .ci-local/install-msvc-deps.py
+++ .ci-local/install-msvc-deps.py
@@ -21,25 +21,38 @@ cachedir = cue.ci['cachedir']
 if cue.ci['os'] == 'windows' and os.sep == '/':
     cachedir = cachedir.replace('\\', '/')
 
+libxml2 = 'libxml2-2.9.3'
+iconv = 'iconv-1.14'
+
+dirs = {}
+dirs[libxml2] = os.path.join(cachedir, libxml2)
+dirs[iconv] = os.path.join(cachedir, iconv)
+
 if 'OPEN62541' in os.environ:
     with open(os.path.join(curdir, 'configure', 'CONFIG_SITE.local'), 'a') as f:
         f.write('''
 LIBXML2 = {0}
 ICONV = {1}
-'''.format(os.path.join(cachedir, 'libxml2-2.9.3'), os.path.join(cachedir, 'iconv-1.14')))
+'''.format(dirs[libxml2], dirs[iconv]))
 
     print('Wrote {0} as'.format(os.path.join(curdir, 'configure', 'CONFIG_SITE.local')))
     with open(os.path.join(curdir, 'configure', 'CONFIG_SITE.local'), 'r') as f:
         print(f.read())
 
-    files = [ 'libxml2-2.9.3-win32-x86_64.7z', 'iconv-1.14-win32-x86_64.7z' ]
-    print('Downloading Windows dependencies ...')
+    files = [ libxml2, iconv ]
+
+    print('Downloading Windows MSVC dependencies ... libxml2 and iconv')
     sys.stdout.flush()
     for f in files:
+      tar = f + '-win32-x86_64.7z'
+      dir = dirs[f]
+
+      try:
+          os.makedirs(dir)
+      except:
+          pass
+
       sp.check_call(['curl', '-fsSL', '--retry', '3', '-O',
-                     'http://xmlsoft.org/sources/win32/64bit/{0}'.format(f)],
+                     'http://xmlsoft.org/sources/win32/64bit/{0}'.format(tar)],
                     cwd=cue.toolsdir)
-      sp.check_call(['7z', 'x', '-aoa', '-bd', os.path.join(cue.toolsdir, f)], cwd=cachedir)
-
-
-      sp.check_call(['dir'], cwd=cachedir)
+      sp.check_call(['7z', 'x', '-aoa', '-bd', os.path.join(cue.toolsdir, tar)], cwd=dir)
