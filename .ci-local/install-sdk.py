0047e50bd3154c952e479bf8def58941c40cc7eb
Ralph Lange <ralph.lange@gmx.de>
2024-07-24 16:32:06 +0200

    ci: add Windows downloads for UASDK
    - add settings for third-party libraries

--- .ci-local/install-sdk.py
+++ .ci-local/install-sdk.py
@@ -32,6 +32,10 @@ UASDK_LIB_DIR = $(UASDK)/lib
 UASDK_SHRLIB_DIR = $(UASDK)/bin
 UASDK_USE_CRYPTO = YES
 UASDK_USE_XMLPARSER = YES'''.format(sdkdir))
+        if cue.ci['os'] == 'windows':
+            f.write('''
+LIBXML2 = $(UASDK)/third-party/win64/vs2015/libxml2
+OPENSSL = $(UASDK)/third-party/win64/vs2015/openssl''')
 
     if os.environ['UASDK'].startswith('1.5.'):
         uasdkcc = 'gcc4.7.2'
@@ -44,8 +48,12 @@ UASDK_USE_XMLPARSER = YES'''.format(sdkdir))
     except:
         pass
 
-    tar_name = 'uasdk-x86_64-{0}-{1}.tar.gz'.format(uasdkcc, os.environ['UASDK'])
+    if cue.ci['os'] == 'windows':
+        tar_name = 'uasdk-win64-vs2015-{0}.zip'.format(os.environ['UASDK'])
+    else:
+        tar_name = 'uasdk-x86_64-{0}-{1}.tar.gz'.format(uasdkcc, os.environ['UASDK'])
     enc_name = tar_name + '.enc'
+
     print('Downloading UA SDK {0}'.format(os.environ['UASDK']))
     sys.stdout.flush()
     sp.check_call(['curl', '-fsSL', '--retry', '3', '-o', enc_name,
@@ -55,8 +63,14 @@ UASDK_USE_XMLPARSER = YES'''.format(sdkdir))
     sp.check_call(['openssl', 'aes-256-cbc', '-md', 'md5',
                    '-k', os.environ['encrypted_178ee45b7f75_pass'],
                    '-in', enc_name, '-out', tar_name, '-d'], cwd=cue.toolsdir)
+
     if os.path.exists(sdkdir):
         shutil.rmtree(sdkdir)
-    sp.check_call(['tar', '-C', sourcedir, '-xz', '-f', os.path.join(cue.toolsdir, tar_name)])
+
+    if cue.ci['os'] == 'windows':
+        sp.check_call(['7z', 'x', '-aoa', '-bd', os.path.join(cue.toolsdir, tar_name)], cwd=sourcedir)
+    else:
+        sp.check_call(['tar', '-C', sourcedir, '-xz', '-f', os.path.join(cue.toolsdir, tar_name)])
+
     os.remove(os.path.join(cue.toolsdir, enc_name))
     os.remove(os.path.join(cue.toolsdir, tar_name))
